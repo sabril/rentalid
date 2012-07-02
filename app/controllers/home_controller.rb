d90feb6d18268c70d16f85324bd6479ad563c628
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2012-07-02 21:12:42 +0700

    order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.order("RANDOM()").limit(8)
+    @featured_product = Product.order("RANDOM()").first
   end
 end
